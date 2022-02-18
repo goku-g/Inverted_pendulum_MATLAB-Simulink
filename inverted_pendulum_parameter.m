a50cb9e4dcdd0141a6bde7fdc69de48d390c256e
goku-g <gokarnabaskota500@gmail.com>
2022-02-18 18:25:20 +0545

    modeling and simulation

---
+++ inverted_pendulum_parameter.m
@@ -0,0 +1,10 @@
+clear
+%% parameters for inverted pendulum
+M = 0.5;    % mass of the chassis  
+m = 0.2;  % mass of the wheels and shaft
+b = 0.1;  % estimate of viscous friction coefficient (N-m-s)
+I = 0.0006;% moment of inertia of the pendulum
+g = 9.8;  %acceleration due to gravity (m/s^2)
+l = 0.3;  %length to pendulum center of mass
+
+%% new section
