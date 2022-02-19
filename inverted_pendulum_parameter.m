9f1c184bd41215e1d9c3df7633aebeb25d8f1800
goku-g <gokarnabaskota500@gmail.com>
2022-02-20 01:02:07 +0545

    update parameters

--- inverted_pendulum_parameter.m
+++ inverted_pendulum_parameter.m
@@ -3,8 +3,7 @@ clear
 M = 0.5;    % mass of the chassis  
 m = 0.2;  % mass of the wheels and shaft
 b = 0.1;  % estimate of viscous friction coefficient (N-m-s)
-I = 0.0006;% moment of inertia of the pendulum
+I = 0.006;% moment of inertia of the pendulum
 g = 9.8;  %acceleration due to gravity (m/s^2)
 l = 0.3;  %length to pendulum center of mass
-
 %% new section
